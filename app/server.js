b1eb7075bf53375bcc2239438ad7df067ecdc92a
vasanthanp <vasanthan322001@gmail.com>
2020-11-05 13:05:34 +0530

    Add put and delete method

--- app/server.js
+++ app/server.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const { getMovies, storeMovies } = require("./service/movies");
+const {
+  getMovies,
+  storeMovies,
+  getMovieWithId,
+  updateMovieWithId,
+  deleteMovieWithId,
+} = require("./service/movies");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.get("/movies", async (req, res) => {
-  let moviesList = await getMovies();
-  res.send(moviesList);
+
+app.get("/movies", (req, res) => {
+  getMovies(res);
 });
+
 app.post("/movies", (req, res) => {
   const { body } = req;
-  storeMovies(body);
-  res.status(404).redirect("/movies");
+  storeMovies(body, res);
+});
+app.get("/movie/:id", (req, res) => {
+  const { id } = req.params;
+  getMovieWithId(id, res);
+});
+
+app.put("/movie/:id", (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  updateMovieWithId(id, body, res);
+});
+app.delete("/movie/:id", (req, res) => {
+  const { id } = req.params;
+  deleteMovieWithId(id, res);
 });
 app.listen(3000, function () {
   console.log("listening on 3000");
--- app/service/movies.js
+++ app/service/movies.js
@@ -4,33 +4,70 @@ const uri =
   "mongodb+srv://vasan:D2RDFoK7jjcQervL@cluster0.uzn2s.mongodb.net/omdb?retryWrites=true&w=majority";
 const connectToDb = async () =>
   MongoClient.connect(uri, { useUnifiedTopology: true });
-const getMovies = async () => {
+const getMovies = async (res) => {
   try {
-    console.log("success");
     const client = await connectToDb();
     const db = client.db("omdb");
     const response = await db.collection("movies").find({});
-    return response.toArray();
+    const movies = await response.toArray();
+    res.send(movies);
   } catch (error) {
     console.log("Error" + error);
+    return res.status(404).json({ Error: "File not found" });
   }
 };
-const storeMovies = async (data) => {
+const storeMovies = async (data, res) => {
+  try {
+    const client = await connectToDb();
+    const db = client.db("omdb");
+    const response = await db.collection("movies").insertOne(data);
+    res.redirect("/movies");
+  } catch (error) {
+    console.log("Error" + error);
+    return res.status(404).json({ Error: "File not found" });
+  }
+};
+
+const getMovieWithId = async (id, res) => {
+  try {
+    const client = await connectToDb();
+    const db = client.db("omdb");
+    const response = await db.collection("movies").findOne({ imdbID: id });
+    res.send(response);
+  } catch (error) {
+    console.log("Error" + error);
+    return res.status(404).json({ Error: "File not found" });
+  }
+};
+const updateMovieWithId = async (id, data, res) => {
   try {
     const client = await connectToDb();
     const db = client.db("omdb");
     const response = await db
       .collection("movies")
-      .insertOne(data, (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(result);
-        }
-      });
+      .updateOne({ imdbID: id }, { $set: data });
+    res.redirect(`/movie/${id}`);
   } catch (error) {
     console.log("Error" + error);
+    return res.status(404).json({ Error: "File not found" });
   }
 };
 
-module.exports = { getMovies: getMovies, storeMovies: storeMovies };
+const deleteMovieWithId = async (id, res) => {
+  try {
+    const client = await connectToDb();
+    const db = client.db("omdb");
+    const response = await db.collection("movies").deleteOne({ imdbID: id });
+    res.redirect("/movies");
+  } catch (error) {
+    console.log("Error" + error);
+    return res.status(404).json({ Error: "File not found" });
+  }
+};
+module.exports = {
+  getMovies: getMovies,
+  storeMovies: storeMovies,
+  getMovieWithId: getMovieWithId,
+  updateMovieWithId: updateMovieWithId,
+  deleteMovieWithId: deleteMovieWithId,
+};
