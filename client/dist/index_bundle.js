d9bd53f464ac5acce7069913227912725c1fdb0e
Ramya Chinnadurai <ramyachinnadurai9626@gmail.com>
2020-11-06 14:19:53 +0530

    Add - react base setup

---
+++ client/dist/index_bundle.js
@@ -0,0 +1,17 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/ramya/Documents/Workspace/movieinfo/client/src/index.js: Support for the experimental syntax 'jsx' isn't currently enabled (6:17):\\n\\n\\u001b[0m \\u001b[90m 4 | \\u001b[39m\\u001b[36mimport\\u001b[39m \\u001b[33mApp\\u001b[39m from \\u001b[32m'./App'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 | \\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 6 | \\u001b[39m\\u001b[33mReactDOM\\u001b[39m\\u001b[33m.\\u001b[39mrender(\\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m'root'\\u001b[39m))\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   | \\u001b[39m                \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:790:17)\\n    at Parser.raiseWithData (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:783:17)\\n    at Parser.expectOnePlugin (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:9110:18)\\n    at Parser.parseExprAtom (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:10523:22)\\n    at Parser.parseExprSubscripts (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:10094:23)\\n    at Parser.parseUpdate (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:10074:21)\\n    at Parser.parseMaybeUnary (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:10063:17)\\n    at Parser.parseExprOps (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:9933:23)\\n    at Parser.parseMaybeConditional (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:9907:23)\\n    at Parser.parseMaybeAssign (/Users/ramya/Documents/Workspace/movieinfo/client/node_modules/@babel/parser/lib/index.js:9870:21)\");\n\n//# sourceURL=webpack://moviebazar/./src/index.js?");
+/******/ })()
+;
