066542fd654a0a57a49183948abaf21dd3001425
karthikeyan <karthikepsilon@gmail.com>
2020-11-06 14:32:46 +0530

    Add favourite button

--- public/js/main.js
+++ public/js/main.js
@@ -28,10 +28,10 @@ function loadMovies(movies){
   var output = '';
   movies.forEach(function(movie){
       output += `
-      <div class="container">
+      <div class="container" id="${movie.imdbID}">
         <div>
           <img class="object-fit-cover" src="${movie.Poster}">
-          <h2 class="movie-title" >${movie.Title}</h2>
+          <h2 class="movie-title" >${movie.Title} <svg data-favourite="${Boolean(movie.Favourite)}" class="${movie.Favourite ? 'favourite-on': 'favourite-off'}" onclick="toggleFavourite('${movie.imdbID}')" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#DC143C" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></h2>
           <a class="view-details" onclick="movieSelected('${movie.imdbID}')" target="_blank" >Movie Details</a>
         </div>
       </div>
@@ -39,6 +39,14 @@ function loadMovies(movies){
   })
   
   document.getElementById('movies').innerHTML = output;
+}
+
+function toggleFavourite(id){
+  const previousValue = document.querySelector(`#${id} svg`).getAttribute('data-favourite');
+  const newValue = previousValue === "false" ? "true" : "false";
+  const className = newValue === "false" ? "favourite-off" : "favourite-on";
+  document.querySelector(`#${id} svg`).setAttribute('data-favourite', newValue);
+  document.querySelector(`#${id} svg`).setAttribute('class', className);
 }
 
 function movieSelected(id){
